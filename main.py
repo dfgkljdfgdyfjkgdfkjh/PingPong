bc0bb3b2544cf5369d583b5433f95059234ba6a2
dfgkljdfgdyfjkgdfkjh <148703133+dfgkljdfgdyfjkgdfkjh@users.noreply.github.com>
2023-11-05 10:47:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ FPS = 60
 
 racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
 racket2 = Player('racket.png', 520, 200, 4, 50, 150)
-ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+ball = GameSprite('tennis.png', 200, 200, 4, 50, 50)
 
 
 font.init()
